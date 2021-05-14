1744a48e3c3213343bb4a552efdee6e79dd61e1e
JLiu13-Emily <mvsunshine15@gmail.com>
2021-05-13 20:30:13 -0700

    add newsfeeds tests

--- testing/testcases.py
+++ testing/testcases.py
@@ -1,16 +1,27 @@
 from django.test import TestCase as DjangoTestCase
 from django.contrib.auth.models import User
 from tweets.models import Tweet
+from rest_framework.test import APIClient
+
 
 class TestCase(DjangoTestCase):
+
+    @property
+    def anonymous_client(self):
+        if hasattr(self, '_anonymous_client'):
+            return self._anonymous_client
+        self._anonymous_client = APIClient()
+        return self._anonymous_client
+
     def create_user(self, username, email=None, password=None):
         if password is None:
             password = 'generic password'
         if email is None:
             email = f'{username}@twitter.com'
-        #不能写成 USer.objects.create()
-        #因为 password需要被加密，username和email需要进行一些normalize
+        # 不能写成 USer.objects.create()
+        # 因为 password需要被加密，username和email需要进行一些normalize
         return User.objects.create_user(username, email, password)
+
     def create_tweet(self, user, content=None):
         if content is None:
             content = 'default tweet content'
